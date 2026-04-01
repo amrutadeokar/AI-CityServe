3601fce68e49bcf8aa00b0b1fe0f43ff4c67b81e
Amruta Deokar <adeokar2004@gmail.com>
2026-04-01 23:06:12 +0530

    Uncomment MongoDB connection and add feedback collection

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,4 @@
-# from pymongo import MongoClient
 
-# MONGO_URI = "mongodb://localhost:27017"
-
-# client = MongoClient(MONGO_URI)
-
-# db = client["ai_cityserve"]
-
-# users_collection = db["users"]
-# complaints_collection = db["complaints"]
-
-# requests_collection = db["service_requests"]
 
 from pymongo import MongoClient
 
@@ -22,5 +11,4 @@ users_collection = db["users"]
 complaints_collection = db["complaints"]
 requests_collection = db["service_requests"]
 
-# NEW COLLECTION
 feedback_collection = db["feedback"]
